0e30a9330441433e3c0b123fe468d510cb5236fb
hemendrasahu67 <57944144+hemendrasahu67@users.noreply.github.com>
2024-07-06 09:50:16 +0530

    Update pyroplug.py

--- ggn/assets/pyroplug.py
+++ ggn/assets/pyroplug.py
@@ -39,7 +39,7 @@ async def copy_message_with_chat_id(client, sender, chat_id, message_id):
         for word, replace_word in replacements.items():
             final_caption = final_caption.replace(word, replace_word)
         
-        caption = f"{final_caption}\n\n__**{custom_caption}**__" if custom_caption else f"{final_caption}\n\n__**[Team SPY](https://t.me/devggn)**__"
+        caption = f"{final_caption}\n\n__**{custom_caption}**__" if custom_caption else f"{final_caption}\n\n__**[🐲 𝗗𝗥𝗔𝗚𝗢𝗡 𝗭𝗢𝗡𝗘 🐲](https://t.me/+zPqF46rm7YU3ODM9)**__"
         
         if msg.media:
             if msg.media == MessageMediaType.VIDEO:
@@ -106,7 +106,7 @@ async def send_video_with_chat_id(client, sender, path, caption, duration, hi, w
             progress=progress_for_pyrogram,
             progress_args=(
                 client,
-                '**__Uploading: [Team SPY](https://t.me/devggn)__**\n ',
+                '**__Uploading: [🐲 𝗗𝗥𝗔𝗚𝗢𝗡 𝗭𝗢𝗡𝗘 🐲](https://t.me/+zPqF46rm7YU3ODM9)__**\n ',
                 upm,
                 time.time()
             )
@@ -132,7 +132,7 @@ async def send_document_with_chat_id(client, sender, path, caption, thumb_path, 
             progress=progress_for_pyrogram,
             progress_args=(
                 client,
-                '**__Uploading:__**\n**__Bot made by [Team SPY](https://t.me/devggn)__**',
+                '**__Uploading:__**\n**__𝗕𝗢𝗧 𝗠𝗔𝗗 𝗕𝗬 ⭐ [🐲 𝗗𝗥𝗔𝗚𝗢𝗡 𝗭𝗢𝗡𝗘 🐲](https://t.me/+zPqF46rm7YU3ODM9)__**',
                 upm,
                 time.time()
             )
@@ -208,8 +208,8 @@ async def _unauth(event):
 # Note you can modify the commands if you need authorization for use and want for commercial purposes...
 # Edit '#' before rename setchat setcaption etc if you want to restrict them for not using this 
 
-API_ID = "19748984" 
-API_HASH = "2141e30f96dfbd8c46fbb5ff4b197004"
+API_ID = "25274255" 
+API_HASH = "02b2832c0e2cc02d8cf178c8a95a5d59"
 
 async def check(userbot, client, link, event):
     logging.info(link)
@@ -251,7 +251,7 @@ async def check(userbot, client, link, event):
         except Exception as e:
             logging.error(e)
             # If user_bot instance fails, fall back to using userbot
-            return False, "Bot is not there add your session to save without link or send invite link...\n\nTo generate session you can use our official bot - @stringsessionAK47bot.."
+            return False, "Bot is not there add your session to save without link or send invite link...\n\nTo generate session you can use our  - @String_session_bot.."
     else:
         try:
             chat = str(link.split("/")[-2])
@@ -357,10 +357,10 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
             edit = await client.edit_message_text(sender, edit_id, "Trying to Download.")
             user_session = load_user_session(sender)
             if user_session:
-              file = await user_bot.download_media(msg, progress=progress_for_pyrogram, progress_args=(client, "**__Unrestricting__: __[Team SPY](https://t.me/devggn)__**\n ", edit, time.time()))
+              file = await user_bot.download_media(msg, progress=progress_for_pyrogram, progress_args=(client, "**__Unrestricting__: __[🐲 𝗗𝗥𝗔𝗚𝗢𝗡 𝗭𝗢𝗡𝗘 🐲](https://t.me/Dragon_zone_owner_bot)__**\n ", edit, time.time()))
               await user_bot.stop()
             else:
-              file = await userbot.download_media(msg, progress=progress_for_pyrogram, progress_args=(client, "**__Unrestricting__: __[Team SPY](https://t.me/devggn)__**\n ", edit, time.time()))            # Retrieve user's custom renaming preference if set, default to '@devggn' otherwise
+              file = await userbot.download_media(msg, progress=progress_for_pyrogram, progress_args=(client, "**__Unrestricting__: __[🐲 𝗗𝗥𝗔𝗚𝗢𝗡 𝗭𝗢𝗡𝗘 🐲](https://t.me/Dragon_zone_owner_bot)__**\n ", edit, time.time()))            # Retrieve user's custom renaming preference if set, default to '@devggn' otherwise
             if not file:
               await client.send_message(sender, "Failed to download the media.")
               return None
@@ -427,7 +427,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
                 replacements = load_replacement_words(sender)
                 for word, replace_word in replacements.items():
                     final_caption = final_caption.replace(word, replace_word)
-                caption = f"{final_caption}\n\n__**{custom_caption}**__" if custom_caption else f"{final_caption}\n\n__**[Team SPY](https://t.me/devggn)**__"
+                caption = f"{final_caption}\n\n__**{custom_caption}**__" if custom_caption else f"{final_caption}\n\n__**[🐲 𝗗𝗥𝗔𝗚𝗢𝗡 𝗭𝗢𝗡𝗘 🐲](https://t.me/Dragon_zone_owner_bot)**__"
                 await send_video_with_chat_id(client, sender, path, caption, duration, hi, wi, thumb_path, upm)
             elif str(file).split(".")[-1] in ['jpg', 'jpeg', 'png', 'webp']:
                 if file_n != '':
@@ -471,7 +471,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i, file_n):
             await upm.delete()
             return None
         except (ChannelBanned, ChannelInvalid, ChannelPrivate, ChatIdInvalid, ChatInvalid):
-            await client.edit_message_text(sender, edit_id, "Bot is not in that channel/group \nsend the invite or add session vioa command /addsession link so that bot can join the channel\n\nTo generate session you can use our official bot - @stringsessionAK47bot")
+            await client.edit_message_text(sender, edit_id, "Bot is not in that channel/group \nsend the invite or add session vioa command /addsession link so that bot can join the channel\n\nTo generate session you can use our bot - @String_session_bot")
             return None
     else:
         edit = await client.edit_message_text(sender, edit_id, "Cloning.")
@@ -612,7 +612,7 @@ async def settings_command(event):
         [Button.inline("Remove Words", b'delete'), Button.url("Developer", 'https://t.me/ggnhere')],
         [Button.inline("Login", b'addsession'), Button.inline("Logout", b'logout')],
         [Button.inline("Set Thumbnail", b'setthumb'), Button.inline("Remove Thumbnail", b'remthumb')],
-        [Button.url("Repo Link", "https://github.com/devgaganin/Save-Restricted-Content-Bot-Repo/")]
+        [Button.url("Repo Link", "https://t.me/Dragon_zone_owner_bot")]
     ]
     
     await teamspy.send_file(
